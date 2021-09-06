cf812d5f2d21b3388cf1eaf43cb79c22a149f47f
Eric Lam <53859607+airyclam@users.noreply.github.com>
2021-09-05 15:01:55 -1000

    vaccine form

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -15,6 +15,7 @@ import NotFound from '../pages/NotFound';
 import Signin from '../pages/Signin';
 import Signup from '../pages/Signup';
 import Signout from '../pages/Signout';
+import AddVaccine from '../pages/AddVaccine';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 class App extends React.Component {
@@ -28,6 +29,7 @@ class App extends React.Component {
             <Route path="/signin" component={Signin}/>
             <Route path="/signup" component={Signup}/>
             <Route path="/signout" component={Signout}/>
+            <Route path="/vaccine" component={AddVaccine}/>
             <ProtectedRoute path="/list" component={ListStuff}/>
             <ProtectedRoute path="/add" component={AddStuff}/>
             <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
